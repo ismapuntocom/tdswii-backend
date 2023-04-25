cd459e3704511e5b2ebc998a06ff1b6e49e6f7a3
Fabián Henry <fhenrynk@gmail.com>
2023-04-25 19:15:07 -0400

    Nothing relevant

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,24 @@
 const User = require("../models/User")
 
 async function registerUser (req, res) {
+    
     try {
         const { correo, password } = req.body
         const newUser = await User.create({ correo: correo, password: password, tipo_user: "usuario"})
+
         res.status(201).json(newUser)
+        res.send("pog")
     } 
     catch (error) {
         res.status(400).json({
             error: error.name,
             message: error.message
         })
+        res.send("not pog")
     }
 }
 
 async function loginUser (req, res) {
-
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const { Pool } = require("pg")
+const cors = require("cors")
 const dotenv = require("dotenv")
 const bodyParser = require("body-parser")
 const usersRoutes = require("./routes/users")
@@ -10,11 +10,24 @@ dotenv.config()
 const PORT = process.env.PORT || 5000
 
 // Configurar body-parser para analizar los datos del formulario
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/users", usersRoutes)
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+
 app.listen(PORT, () => {
-    console.log(`Example app linstening at http://localhost:${PORT}`)
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
 
 app.get("/", async (req,res) => {
